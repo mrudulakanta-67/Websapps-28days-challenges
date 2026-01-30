169991215a9a9286cf346a475e8cabbb169834b6
mrudulakanta-67 <mrudulakanta@gmail.com>
2026-01-30 18:14:39 +0530

    challenges

---
+++ day08/script.js
@@ -0,0 +1,14 @@
+let arr=[19,29,84,74,65,66,30]
+console.log(arr);
+let len=arr.length;
+console.log(len);
+console.log(arr[0]);
+console.log(arr[len-1])
+arr[0]=93;
+console.log(arr);
+delete arr[2];
+console.log(arr);
+arr.push(67);
+console.log(arr);
+let array=[6,"orange","super",377,"apple"]
+console.log(array);
